bfcdcc23871fb2fd58b9c47128fde118399245be
kiancliffe <kiancliffe@gmail.com>
2024-09-30 06:27:08 -0400

    Added Docstring

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,7 +2,10 @@ import requests
 import json
 
 def emotion_detector(text_to_analyse):
-    
+    """
+    Sends post request to server for EmotionPredict ai then parses and returns 
+    emotion scores based on the text passed to it.
+    """
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
